915bcbf1f4a6c4ab8b82df032d2c8082155321f8
kbosedbl <kbosedbl@gmail.com>
2020-10-24 01:07:40 +0530

    Changed the dimension of image view

--- app/src/main/java/com/example/connect/Adapter/PostAdapter.java
+++ app/src/main/java/com/example/connect/Adapter/PostAdapter.java
@@ -10,9 +10,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.connect.Model.Post;
+import com.example.connect.Model.User;
 import com.example.connect.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -37,8 +45,22 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        Post post = mPost.get(i);
+        if(post.getPostimage().length() == 0)
+            post.setPostimage("https://ca-times.brightspotcdn.com/dims4/default/bcec4c4/2147483647/strip/true/crop/1537x867+0+0/resize/840x474!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F75%2Fd2%2F00878f5cf6099f9ba222e27f8c08%2Fla-1498786875-nfjyg97v0l-snap-image");
+        Glide.with(mContext).load(post.getPostimage()).into(viewHolder.post_image);
 
+        if(post.getDescription().equals(""))
+        {
+            viewHolder.description.setVisibility(View.GONE);
+        }else{
+            viewHolder.description.setVisibility(View.VISIBLE);
+            viewHolder.description.setText(post.getDescription());
+        }
+
+        publisherInfo(viewHolder.image_profile , viewHolder.username , viewHolder.publisher , post.getPublisher());
     }
 
     @Override
@@ -64,5 +86,24 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             description = itemView.findViewById(R.id.description);
             comments = itemView.findViewById(R.id.comments);
         }
+    }
+
+    private void publisherInfo(final ImageView image_profile , final TextView username , final TextView publisher , String userid){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Glide.with(mContext).load(user.getImageUrl()).into(image_profile);
+                username.setText(user.getUserName());
+                publisher.setText(user.getUserName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
